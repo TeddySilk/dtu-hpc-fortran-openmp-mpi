0187744753bb6e2e69edcb958b62a270d7a338df
Kristian Ebstrup Jacobsen <k.ebstrup@gmail.com>
2022-01-17 15:04:26 +0100

    last version before code restructure

---
+++ exercises/mpi/mpi_heat_diffusion/src/m_global.f90
@@ -0,0 +1,48 @@
+MODULE m_global
+
+   ! ------------------------------------------------- !
+   ! MODULES                                           !
+   ! ------------------------------------------------- !
+
+   IMPLICIT NONE
+
+   ! ------------------------------------------------- !
+   ! INPUT FILE                                        !
+   ! ------------------------------------------------- !
+   CHARACTER(LEN = 24)   :: input_file = 'inputs.txt'
+
+   ! ------------------------------------------------- !
+   ! INPUT PARAMETERS                                  !
+   ! ------------------------------------------------- !
+   INTEGER   :: Nx, Ny                          ! nx: amount of cells in x-dir, ny: amount of cells in y-dir
+   INTEGER   :: Lx, Ly                          ! box dimensions (Lx: length in x-dir, Ly: length in y-dir)
+   INTEGER   :: diff_const                      ! D: diffusion constant
+   INTEGER   :: nsteps                          ! nsteps: amount of time steps made
+   INTEGER   :: diagfreq                        ! diagfreq: amount of time steps made per diagnostic print-out
+   LOGICAL   :: benchmark                       ! benchmark: .TRUE.: benchmarking is performed and output
+   LOGICAL   :: save_bin                        ! save_bin: .TRUE.: binary backups of the field is made
+   INTEGER   :: binfreq                         ! binfreq: amount of time steps made per binary backup
+   REAL      :: Tinit                           ! Tinit: initial field temperature
+   REAL      :: Tboundary                       ! Tboundary: Dirilect-boundary field temperature
+
+   ! ------------------------------------------------- !
+   ! DATA EXTRACT PARAMETERS                           !
+   ! ------------------------------------------------- !
+   CHARACTER(LEN = 24)    :: output_file            ! output_file: Name of the field data output file
+   CHARACTER(LEN = 24)    :: benchmark_file         ! benchmark_file: Name of the benchmark file for CPU times
+   CHARACTER(LEN = 24)    :: binary_file            ! binary_file: Name of the field data binary backup file (for perfect continuation)
+   CHARACTER(LEN = 24)    :: diagnostic_file        ! diagnostic_file: Name of the diagnostic file
+   INTEGER                :: diagnostic_unit        ! diagnostic_file_unit: File-unit for the diagnostic file
+
+   ! ------------------------------------------------- !
+   ! DEPENDENT PARAMETERS                              !
+   ! ------------------------------------------------- !
+   REAL      :: dx          ! dx: cell width in the x-dir
+   REAL      :: dy          ! dy: cell width in the y-dir
+   REAL      :: rdx2        ! rdx2: 1/(dx ** 2), to aid setting up the equations
+   REAL      :: rdy2        ! rdy2: see above
+   REAL      :: dt          ! dt: time-step size, satisfying the Fourier limit
+
+
+END MODULE
+
---
+++ exercises/mpi/mpi_heat_diffusion/src/main.f90
@@ -0,0 +1,114 @@
+PROGRAM main
+   ! ------------------------------------------------- !
+   !  Filename : heat_diffusion_2d                     !
+   !  Version  : 0.5                                   !
+   !  Author   : Kristian Ebstrup Jacobsen             !
+   !  Created  : January 8, 2022                       !
+   ! ------------------------------------------------- !
+
+   ! ------------------------------------------------- !
+   ! MODULES                                           !
+   ! ------------------------------------------------- !
+   USE m_global
+   USE m_read_input
+   USE m_alloc
+   USE m_extract_field
+   USE m_extract_binary
+   USE m_init_tfield
+   USE m_simulate_diffusion
+
+   ! ------------------------------------------------- !
+   ! INITIALIZATION                                    !
+   ! ------------------------------------------------- !
+   IMPLICIT NONE
+
+   ! initialize relevant variables
+   INTEGER              :: info, nargc, i, step = 0
+   CHARACTER(LEN = 24)  :: arg_string, cfield, cstep
+   LOGICAL              :: arg_exists
+
+   ! empty array to contain the temperature field
+   REAL, DIMENSION(:,:), ALLOCATABLE :: tfield
+
+   PRINT*, "# ___________ PROGRAM STARTED ___________ #"
+   PRINT "(A)"
+
+   ! read input file
+   CALL read_input(input_file)
+
+   ! allocate memory to the global field
+   CALL alloc(tfield, Nx, Ny, info)
+
+   ! initialize the temperature field according to BC
+   CALL init_tfield(tfield, boundary_temperature = Tboundary, initial_temperature = Tinit)
+
+   ! ------------------------------------------------- !
+   ! INPUT FIELD                                       !
+   ! ------------------------------------------------- !
+   ! if the path to a binary file for a field is given as a command argument,
+   ! continue from that field.
+   nargc = iargc()
+   IF (nargc.GE.1) THEN
+
+      ! get first argument (file-path)
+      CALL getarg(1, arg_string)
+      INQUIRE(FILE=arg_string, EXIST=arg_exists)
+
+      ! check if the file exists
+      IF (arg_exists) THEN
+         PRINT "(A)", TRIM("CONTINUING FROM GIVEN FIELD INPUT:")
+         i = INDEX(arg_string, "@")
+         cfield = arg_string
+
+         ! read binary field file
+         PRINT "(A, A)", TRIM("    NAME: "), TRIM(cfield)
+         OPEN(80, FILE=TRIM(arg_string), FORM="UNFORMATTED")
+         READ(80, IOSTAT=info) step, tfield
+         PRINT "(A, I8)", TRIM("    STEP: "), step
+         IF (info.EQ.0) THEN
+            PRINT "(A)", TRIM("    IMPORT SUCCESSFUL")
+         ELSE
+            PRINT "(A, I8)", TRIM("    IMPORT FAILED. ERROR CODE: "), info
+            PRINT "(A)"
+            PRINT*, "# ___________ PROGRAM EXITED ___________ #"
+            STOP
+         ENDIF
+
+         ! if the file doens't exist, exit the program and complain
+      ELSE
+         PRINT "(A)", TRIM("GIVEN FIELD PATH IS INVALID!")
+         PRINT "(A)"
+         PRINT*, "# ___________ PROGRAM EXITED ___________ #"
+         STOP
+
+      ENDIF
+
+   ENDIF
+
+   ! ------------------------------------------------- !
+   ! HEAT DIFFUSION                                    !
+   ! ------------------------------------------------- !
+   IF (benchmark) THEN
+      DO i = 1, 20
+         CALL simulate_diffusion(tfield, nsteps, &
+            input_step = step, verbose = 0, &
+            binary_backup = save_bin, benchmarking = benchmark)
+      ENDDO
+   ELSE
+      CALL simulate_diffusion(tfield, nsteps, &
+         input_step = step, verbose = 0, &
+         binary_backup = save_bin, benchmarking = benchmark)
+   ENDIF
+
+   ! ------------------------------------------------- !
+   ! EXTRACT FIELD(S)                                  !
+   ! ------------------------------------------------- !
+   !CALL extract_field(tfield, output_file, nsteps + step)
+   IF (save_bin) THEN
+      CALL extract_binary(tfield, binary_file, nsteps + step)
+   ENDIF
+
+   PRINT "(A)"
+   PRINT*, "# ___________ PROGRAM SUCCESSFUL ___________ #"
+
+END PROGRAM main
