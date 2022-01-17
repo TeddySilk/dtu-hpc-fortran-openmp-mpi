4e8b1e366ca965f261ff3d811326d65657f9e0cc
Kristian Ebstrup Jacobsen <k.ebstrup@gmail.com>
2022-01-17 09:51:16 +0100

    updated readme

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# 41391_hpc_fortran_openmp_mpi
+# 41391 - HIGH PERFORMACNE COMPUTING: FORTRAN, OPENMP, and MPI
 This repo contains the exercise solutions and project files for the PhD course "41391 High Performance Computing: Fortran, OpenMP and MPI" given at DTU.
 
 Excuse the dust, as it is a work-in-progress.
+
+# TO-DO LIST
+A to-do list is kept here for future reference to track outstanding issues in the repo.
+- [ ] \(exercises/openmp/poisson) Implement OpenMP properly in the Poisson problem exercise, and set-up easy benchmarking, including CPU up-time extraction.
--- exercises/openmp/poisson/quickplot.sh
+++ exercises/openmp/poisson/quickplot.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 Data="./res/Tfield.dat"
+fontsize=18
 if [[ $1 == "png" ]]
 then
     gnuplot-x11 -e "set palette rgb 33,13,10; splot '$Data' u (\$1==-0.5 ?\$1:NaN):2:3:4 w p pt 7 ps 2 palette notitle; set xrange [-1: 1]; set yrange [-1: 1]; set zrange[-1: 1]; set term png; set out 'Tfield.png'; repl"
     okular Tfield.png
 else
-    gnuplot-x11 -e "set palette rgb 33,13,10; splot '$Data' u (\$1<=1.0 && \$1>=-1.0?\$1:NaN):(\$2<=1.0 && \$2>=-1.0?\$2:NaN):3:(\$4>0.0 && \$4<19.0?\$4:NaN) w p pt 7 ps 2 palette notitle; set term post eps color solid; set out 'Tfield.eps'; repl"
+    gnuplot-x11 -e "set palette rgb 33,13,10; splot '$Data' u (\$1<=1.0 && \$1>=-1.0?\$1:NaN):(\$2<=1.0 && \$2>=-1.0?\$2:NaN):(\$3>=-0.1 && \$3<=0.1?\$3:NaN):(\$4>0.0 && \$4<20.0?\$4:NaN) w p pt 7 ps 2 palette notitle; set grid; set cblabel 'T' font ',$fontsize'; set xlabel 'x' font ',$fontsize'; set ylabel 'y' font ',$fontsize'; set zlabel 'z' font ',$fontsize'; set term post eps color solid; set xtics font ', $fontsize'; set ytics font ', $fontsize'; set ztics font ', $fontsize'; set cbtics font ', $fontsize'; set out 'Tfield.eps'; repl"
     okular Tfield.eps
 fi
